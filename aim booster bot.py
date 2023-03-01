98c8bf2e27ee63c32e8a382f1e745204d9d34fc3
charan101010 <126458847+charan101010@users.noreply.github.com>
2023-03-01 19:24:21 +0530

    Create aim booster bot.py

---
+++ aim booster bot.py
@@ -0,0 +1,35 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+time.sleep(2)
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+#Color of center: (255, 219, 195)
+
+while keyboard.is_pressed('q') == False:
+    flag = 0
+    pic = pyautogui.screenshot(region=(497, 302, 1000,1000))
+
+    width, height = pic.size
+
+    for x in range(0, width, 5):
+        for y in range(0, height, 5):
+
+            r, g, b = pic.getpixel((x, y))
+
+            if b == 195 and r == 255 and g == 219:
+                flag = 1
+                click(x+497, y+302)
+                time.sleep(0.008)
+                break
+
+        if flag == 1:
+            break
